05a132c951fea2cd5036822cfb3c7d972dce8a50
Debkanta Dey <debkanta.starwaywebdigital@gmail.com>
2026-09-09 19:01:10 +0530

    Successfully running

--- lib/custom/custom_method/custom_format_date.dart
+++
@@ -1,8 +0,0 @@
-import 'package:intl/intl.dart';
-
-class CustomFormatDate {
-  static String convert(String date) {
-    DateTime convertToDate = DateTime.parse(date);
-    return DateFormat('dd-MM-yy').format(convertToDate);
-  }
-}
--- lib/pages/profile_page/premium_plan_page/payment_controller.dart
+++
@@ -1,5 +0,0 @@
-import 'package:get/get.dart';
-
-class PaymentController extends GetxController {
-  String? paymentMethod;
-}
